9d7cc0f052092c1183d891c94b0924f11e71b9ca
cmsjy040317 <cmsjy040317@naver.com>
2025-01-31 14:33:47 +0900

    Loavalue Project

---
+++ src/test/java/com/lostArk/LoaValue/LoaValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostArk.LoaValue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
